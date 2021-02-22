156948bc51ff49eca4172acb1b544df97ee53503
pcollins2USI <47487358+pcollins2USI@users.noreply.github.com>
2021-02-22 15:39:50 -0600

    Camera.py is more object oriented

--- face_detector/camera_object.py
+++ face_detector/camera_object.py
@@ -56,6 +56,8 @@ class InputBox:
         screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
         # Blit the rect.
         pg.draw.rect(screen, self.color, self.rect, 2)
+        
+#----------------------------------------------------------------------
 
 def make_audio_message(message):
     # Language in which you want to convert 
@@ -67,7 +69,7 @@ def text_objects(text, font):
     textSurface = font.render(text, True, (0,0,0))
     return textSurface, textSurface.get_rect()
 
-
+#----------------------------------------------------------------------
 class button():
     def __init__(self, color, x,y,width,height, text=''):
         self.color = color
@@ -85,7 +87,7 @@ class button():
         pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
         
         if self.text != '':
-            font = pygame.font.SysFont('comicsans', 30)
+            font = pygame.font.SysFont('comicsans', 25)
             text = font.render(self.text, 1, (0,0,0))
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
@@ -95,41 +97,47 @@ class button():
             if pos[1] > self.y and pos[1] < self.y + self.height:
                 return True      
         return False
+    
+#-----------------------------------------------------------------------
+class LoadJson:
+    def __init__(self, jsonFile):
+        with open(jsonFile,'r') as json_config:
+            self.config = json.load(json_config)
+            self.camera_input = int(self.config["camera_input"])
+            self.face_xml_cascade = self.config["face_xml_cascade"]
+            self.eye_xml_cascade = self.config["eye_xml_cascade"]
+            self.smile_xml_cascade = self.config["smile_xml_cascade"]
+            self.face_box_color = (int(self.config["face_box_color"].split(",")[0]),int(self.config["face_box_color"].split(",")[1]),int(self.config["face_box_color"].split(",")[2]))
+            self.eye_box_color = (int(self.config["eye_box_color"].split(",")[0]),int(self.config["eye_box_color"].split(",")[1]),int(self.config["eye_box_color"].split(",")[2]))
+            self.smile_box_color = (int(self.config["smile_box_color"].split(",")[0]),int(self.config["smile_box_color"].split(",")[1]),int(self.config["smile_box_color"].split(",")[2]))
+            self.font_size_of_label = int(self.config["font_size_of_label"])
+            self.color_of_face_label = (int(self.config["color_of_face_label"].split(",")[0]),int(self.config["color_of_face_label"].split(",")[1]),int(self.config["color_of_face_label"].split(",")[2]))
+            self.color_of_eye_label = (int(self.config["color_of_eye_label"].split(",")[0]),int(self.config["color_of_eye_label"].split(",")[1]),int(self.config["color_of_eye_label"].split(",")[2]))
+            self.color_of_smile_label = (int(self.config["color_of_smile_label"].split(",")[0]),int(self.config["color_of_smile_label"].split(",")[1]),int(self.config["color_of_smile_label"].split(",")[2]))
+            self.line_thickness_of_boxes = int(self.config["line_thickness_of_boxes"])
+            self.font_thickness_of_label = int(self.config["font_thickness_of_label"])
+            self.adjust_label_x = int(self.config["adjust_label_x"])
+            self.adjust_label_y = int(self.config["adjust_label_y"])
+            self.pygame_window_name = self.config["pygame_window_name"]
+            self.screen_added_width = int(self.config["screen_added_width"])
+            self.screen_added_height = int(self.config["screen_added_height"])
+            
+#----------------------------------------------------------------------
 
-
-
-if __name__ == "__main__":
-    with open('face_detector_config.json','r') as json_config:
-        config = json.load(json_config)
-        camera_input = int(config["camera_input"])
-        face_xml_cascade = config["face_xml_cascade"]
-        eye_xml_cascade = config["eye_xml_cascade"]
-        smile_xml_cascade = config["smile_xml_cascade"]
-        face_box_color = (int(config["face_box_color"].split(",")[0]),int(config["face_box_color"].split(",")[1]),int(config["face_box_color"].split(",")[2]))
-        eye_box_color = (int(config["eye_box_color"].split(",")[0]),int(config["eye_box_color"].split(",")[1]),int(config["eye_box_color"].split(",")[2]))
-        smile_box_color = (int(config["smile_box_color"].split(",")[0]),int(config["smile_box_color"].split(",")[1]),int(config["smile_box_color"].split(",")[2]))
-        font_size_of_label = int(config["font_size_of_label"])
-        color_of_face_label = (int(config["color_of_face_label"].split(",")[0]),int(config["color_of_face_label"].split(",")[1]),int(config["color_of_face_label"].split(",")[2]))
-        color_of_eye_label = (int(config["color_of_eye_label"].split(",")[0]),int(config["color_of_eye_label"].split(",")[1]),int(config["color_of_eye_label"].split(",")[2]))
-        color_of_smile_label = (int(config["color_of_smile_label"].split(",")[0]),int(config["color_of_smile_label"].split(",")[1]),int(config["color_of_smile_label"].split(",")[2]))
-        line_thickness_of_boxes = int(config["line_thickness_of_boxes"])
-        font_thickness_of_label = int(config["font_thickness_of_label"])
-        adjust_label_x = int(config["adjust_label_x"])
-        adjust_label_y = int(config["adjust_label_y"])
-        pygame_window_name = config["pygame_window_name"]
-        
+if __name__ == "__main__":    
+    settings = LoadJson('face_detector_config.json')   
     start = False
-    count_max = 0
-    count_d = 10
-    capture = cv2.VideoCapture(camera_input)
+    #count_max = 0
+    #count_d = 10
+    capture = cv2.VideoCapture(settings.camera_input)
     frame_width = int(capture.get(3))
     frame_height = int(capture.get(4))
     #out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
-    screen = pygame.display.set_mode((frame_width + 0,frame_height + 100))
-    pygame.display.set_caption(pygame_window_name)
-    face_cascade = cv2.CascadeClassifier(face_xml_cascade)
-    eye_cascade = cv2.CascadeClassifier(eye_xml_cascade)
-    smile_cascade = cv2.CascadeClassifier(smile_xml_cascade)
+    screen = pygame.display.set_mode((frame_width + settings.screen_added_width,frame_height + settings.screen_added_height))
+    pygame.display.set_caption(settings.pygame_window_name)
+    face_cascade = cv2.CascadeClassifier(settings.face_xml_cascade)
+    eye_cascade = cv2.CascadeClassifier(settings.eye_xml_cascade)
+    smile_cascade = cv2.CascadeClassifier(settings.smile_xml_cascade)
 
     num = 0
     num2 = 0
@@ -138,7 +146,7 @@ if __name__ == "__main__":
     def redrawWindow():
         greenButton.draw(screen,(0,0,0))
         
-    greenButton = button((192, 192, 192),470,500,150,50, 'Start')
+    greenButton = button((192, 192, 192),470,500,100,40, 'Start')
     while wait:
         pygame.draw.rect(screen,(255,255,255),pygame.Rect(0,0,640,480))
         redrawWindow()
@@ -149,7 +157,7 @@ if __name__ == "__main__":
             if greenButton.isOver(pos):
                 wait = False
                 start = True
-                greenButton = button((192, 192, 192),470,500,150,50, 'Quit')
+                greenButton = button((192, 192, 192),470,500,100,40, 'Quit')
                 time.sleep(1)
         if event.type == pygame.MOUSEMOTION:
             if greenButton.isOver(pos):
@@ -195,15 +203,15 @@ if __name__ == "__main__":
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
-            cv2.rectangle(img,(x,y),(x+w,y+h),face_box_color,line_thickness_of_boxes)
+            cv2.rectangle(img,(x,y),(x+w,y+h),settings.face_box_color,settings.line_thickness_of_boxes)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = img[y:y+h, x:x+w]
             font = cv2.FONT_HERSHEY_PLAIN 
-            img = cv2.putText(img, 'Face', (x+adjust_label_x,y+adjust_label_y), font, font_size_of_label, (color_of_face_label), font_thickness_of_label, cv2.LINE_AA)
+            img = cv2.putText(img, 'Face', (x+settings.adjust_label_x,y+settings.adjust_label_y), font, settings.font_size_of_label, (settings.color_of_face_label), settings.font_thickness_of_label, cv2.LINE_AA)
             smile = smile_cascade.detectMultiScale(roi_color, 1.1, 40)
             for (sx,sy,sw,sh) in smile:
-                cv2.rectangle(roi_color,(sx,sy),(sx+sw,sy+sh),smile_box_color,line_thickness_of_boxes)
-                img = cv2.putText(img, 'Smile',(x+sx+adjust_label_x,y+sy+adjust_label_y), font, font_size_of_label, (color_of_smile_label), font_thickness_of_label, cv2.LINE_AA)
+                cv2.rectangle(roi_color,(sx,sy),(sx+sw,sy+sh),settings.smile_box_color,settings.line_thickness_of_boxes)
+                img = cv2.putText(img, 'Smile',(x+sx+settings.adjust_label_x,y+sy+settings.adjust_label_y), font, settings.font_size_of_label, (settings.color_of_smile_label), settings.font_thickness_of_label, cv2.LINE_AA)
                 
             eyes = eye_cascade.detectMultiScale(roi_gray)
             if(len(eyes) > 0):
@@ -211,8 +219,8 @@ if __name__ == "__main__":
             else:
                 eye_detected = False
             for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),eye_box_color,line_thickness_of_boxes)
-                img = cv2.putText(img, 'Eye',(x+ex+adjust_label_x,y+ey+adjust_label_y), font, font_size_of_label, (color_of_eye_label), font_thickness_of_label, cv2.LINE_AA)
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),settings.eye_box_color,settings.line_thickness_of_boxes)
+                img = cv2.putText(img, 'Eye',(x+ex+settings.adjust_label_x,y+ey+settings.adjust_label_y), font, settings.font_size_of_label, (settings.color_of_eye_label), settings.font_thickness_of_label, cv2.LINE_AA)
         # Window name in which image is displayed
         num = len(faces)
         cv2.imwrite('1.png',img)
@@ -236,16 +244,16 @@ if __name__ == "__main__":
             screen.blit(faceIcon,(0,480))
             pygame.draw.rect(screen,(255,255,255),pygame.Rect(0,480,100,580),1)
             pygame.display.update()
-            
-        if(count_max < num):
-            #os.system("mpg321 welcome.mp3")
-            #process = Popen(['mpg321', 'welcome.mp3'], stdout=PIPE, stderr=PIPE)
-            count_max = num
-        elif(0 == int(len(faces))):
-            count_d -= 1
-            if (count_d == 0):
-                count_max = 0
-                count_d = 10
+# Audio mp3 played when face is detected            
+#         if(count_max < num):
+#             #os.system("mpg321 welcome.mp3")
+#             #process = Popen(['mpg321', 'welcome.mp3'], stdout=PIPE, stderr=PIPE)
+#             count_max = num
+#         elif(0 == int(len(faces))):
+#             count_d -= 1
+#             if (count_d == 0):
+#                 count_max = 0
+#                 count_d = 10
                 
         if (eye_detected and num >= 1):
             eyeIcon = pygame.image.load('eye.png')
