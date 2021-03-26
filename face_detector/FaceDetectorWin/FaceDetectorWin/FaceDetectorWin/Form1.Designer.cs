db32541e7104d967aaa8ea21f3858c26bce49aa4
Rachael Collins <rlcollins@eagles.usi.edu>
2021-03-26 17:51:14 -0500

    gui C# for windows

---
+++ face_detector/FaceDetectorWin/FaceDetectorWin/FaceDetectorWin/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace FaceDetectorWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBt = new System.Windows.Forms.Button();
+            this.stopBt = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startBt
+            // 
+            this.startBt.BackColor = System.Drawing.Color.White;
+            this.startBt.Location = new System.Drawing.Point(655, 7);
+            this.startBt.Name = "startBt";
+            this.startBt.Size = new System.Drawing.Size(100, 50);
+            this.startBt.TabIndex = 0;
+            this.startBt.Text = "Start";
+            this.startBt.UseVisualStyleBackColor = false;
+            this.startBt.Click += new System.EventHandler(this.startBt_Click);
+            // 
+            // stopBt
+            // 
+            this.stopBt.BackColor = System.Drawing.Color.White;
+            this.stopBt.Location = new System.Drawing.Point(653, 63);
+            this.stopBt.Name = "stopBt";
+            this.stopBt.Size = new System.Drawing.Size(100, 50);
+            this.stopBt.TabIndex = 1;
+            this.stopBt.Text = "Stop";
+            this.stopBt.UseVisualStyleBackColor = false;
+            this.stopBt.Click += new System.EventHandler(this.stopBt_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(769, 542);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.startBt);
+            this.tabPage1.Controls.Add(this.stopBt);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(761, 509);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Face Detector";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(761, 488);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Settings";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(797, 567);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Face Detector GUI";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBt;
+        private System.Windows.Forms.Button stopBt;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ face_detector/FaceDetectorWin/FaceDetectorWin/FaceDetectorWin/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.IO;
+using System.Diagnostics;
+using Xunit;
+
+namespace FaceDetectorWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startBt_Click(object sender, EventArgs e)
+        {
+            cmdCommand();
+            InitTimer();
+        }
+        private System.Timers.Timer timer1;
+        public void InitTimer()
+        {
+            timer1 = new System.Timers.Timer();
+            timer1.Elapsed += new ElapsedEventHandler(timer1_Tick);
+            timer1.Interval = 60; // in miliseconds
+            timer1.Start();
+        }
+        public void cmdCommand()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = @"C:\Users\patri\AppData\Local\Programs\Thonny\python.exe";
+            startInfo.Arguments = @"camera_object_mono.py";
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image != null)
+                {
+                    pictureBox1.Image = null;
+                }
+                else
+                {
+                    var buffer = File.ReadAllBytes("1.png");
+                    pictureBox1.Image = ByteToImage(buffer);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error {0} Error on image update", ex);
+            }
+
+        }
+        public static Bitmap ByteToImage(byte[] blob)
+        {
+            MemoryStream mStream = new MemoryStream();
+            byte[] pData = blob;
+            mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
+            Bitmap bm = new Bitmap(mStream, false);
+            mStream.Dispose();
+            return bm;
+        }
+        private void stopBt_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            foreach (var process in Process.GetProcessesByName("python"))
+            {
+                process.Kill();
+            }
+        }
+    }
+}
