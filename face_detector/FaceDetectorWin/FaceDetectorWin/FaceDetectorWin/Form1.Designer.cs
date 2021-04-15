e4a095fc477c74022c362b79ba04cc6c6c7cf416
Rachael Collins <rlcollins@eagles.usi.edu>
2021-04-15 14:29:27 -0500

    design changes

--- face_detector/FaceDetectorWin/FaceDetectorWin/FaceDetectorWin/Form1.Designer.cs
+++ face_detector/FaceDetectorWin/FaceDetectorWin/FaceDetectorWin/Form1.Designer.cs
@@ -34,16 +34,16 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.gameStopBt = new System.Windows.Forms.Button();
             this.gameStartBt = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -57,10 +57,10 @@
             this.startBt.BackColor = System.Drawing.Color.Maroon;
             this.startBt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.startBt.Font = new System.Drawing.Font("Showcard Gothic", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.startBt.Location = new System.Drawing.Point(661, 6);
-            this.startBt.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.startBt.Location = new System.Drawing.Point(992, 9);
+            this.startBt.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.startBt.Name = "startBt";
-            this.startBt.Size = new System.Drawing.Size(150, 100);
+            this.startBt.Size = new System.Drawing.Size(225, 154);
             this.startBt.TabIndex = 0;
             this.startBt.Text = "Start";
             this.startBt.UseVisualStyleBackColor = false;
@@ -71,10 +71,10 @@
             this.stopBt.BackColor = System.Drawing.Color.Maroon;
             this.stopBt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.stopBt.Font = new System.Drawing.Font("Showcard Gothic", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.stopBt.Location = new System.Drawing.Point(661, 112);
-            this.stopBt.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.stopBt.Location = new System.Drawing.Point(992, 259);
+            this.stopBt.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.stopBt.Name = "stopBt";
-            this.stopBt.Size = new System.Drawing.Size(150, 100);
+            this.stopBt.Size = new System.Drawing.Size(225, 154);
             this.stopBt.TabIndex = 1;
             this.stopBt.Text = "Stop";
             this.stopBt.UseVisualStyleBackColor = false;
@@ -86,11 +86,11 @@
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabControl1.Location = new System.Drawing.Point(11, 12);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.tabControl1.Location = new System.Drawing.Point(16, 18);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(835, 529);
+            this.tabControl1.Size = new System.Drawing.Size(1252, 814);
             this.tabControl1.TabIndex = 2;
             // 
             // tabPage1
@@ -100,21 +100,21 @@
             this.tabPage1.Controls.Add(this.startBt);
             this.tabPage1.Controls.Add(this.stopBt);
             this.tabPage1.Font = new System.Drawing.Font("Showcard Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabPage1.Location = new System.Drawing.Point(4, 29);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 34);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.tabPage1.Size = new System.Drawing.Size(827, 496);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.tabPage1.Size = new System.Drawing.Size(1244, 776);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Face Detector";
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Location = new System.Drawing.Point(7, 6);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(10, 9);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.Size = new System.Drawing.Size(960, 738);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
@@ -128,13 +128,57 @@
             this.tabPage2.Controls.Add(this.comboBox1);
             this.tabPage2.Controls.Add(this.label1);
             this.tabPage2.Font = new System.Drawing.Font("Showcard Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 34);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.tabPage2.Size = new System.Drawing.Size(827, 496);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.tabPage2.Size = new System.Drawing.Size(1244, 776);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Settings";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(327, 148);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(778, 34);
+            this.comboBox3.TabIndex = 5;
+            this.comboBox3.Text = "face_game_csharpReady.py";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 148);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Game Script:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(326, 82);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(778, 34);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.Text = "camera_object_mono.py";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Detector Script:";
             // 
             // comboBox1
             // 
@@ -144,43 +188,42 @@
             "C:\\Users\\chris\\AppData\\Local\\Programs\\Thonny\\python.exe",
             "C:\\Users\\patri\\AppData\\Local\\Programs\\Thonny\\python.exe",
             "C:\\Users\\collinp\\AppData\\Local\\Programs\\Python\\Python37\\python.exe"});
-            this.comboBox1.Location = new System.Drawing.Point(217, 6);
+            this.comboBox1.Location = new System.Drawing.Point(326, 9);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(520, 29);
+            this.comboBox1.Size = new System.Drawing.Size(778, 34);
             this.comboBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(5, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(8, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(207, 21);
+            this.label1.Size = new System.Drawing.Size(248, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Python Executable:";
             // 
             // tabPage3
             // 
+            this.tabPage3.BackColor = System.Drawing.Color.Black;
             this.tabPage3.Controls.Add(this.gameStopBt);
             this.tabPage3.Controls.Add(this.gameStartBt);
             this.tabPage3.Controls.Add(this.pictureBox2);
-            this.tabPage3.Location = new System.Drawing.Point(4, 29);
-            this.tabPage3.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 34);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(827, 496);
+            this.tabPage3.Size = new System.Drawing.Size(1244, 776);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Face Game ";
-            this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // gameStopBt
             // 
             this.gameStopBt.BackColor = System.Drawing.Color.Maroon;
             this.gameStopBt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.gameStopBt.Font = new System.Drawing.Font("Showcard Gothic", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gameStopBt.Location = new System.Drawing.Point(663, 142);
-            this.gameStopBt.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.gameStopBt.Location = new System.Drawing.Point(994, 260);
+            this.gameStopBt.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.gameStopBt.Name = "gameStopBt";
-            this.gameStopBt.Size = new System.Drawing.Size(150, 100);
+            this.gameStopBt.Size = new System.Drawing.Size(225, 154);
             this.gameStopBt.TabIndex = 2;
             this.gameStopBt.Text = "Stop";
             this.gameStopBt.UseVisualStyleBackColor = false;
@@ -191,10 +234,10 @@
             this.gameStartBt.BackColor = System.Drawing.Color.Maroon;
             this.gameStartBt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.gameStartBt.Font = new System.Drawing.Font("Showcard Gothic", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gameStartBt.Location = new System.Drawing.Point(663, 12);
-            this.gameStartBt.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.gameStartBt.Location = new System.Drawing.Point(994, 18);
+            this.gameStartBt.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.gameStartBt.Name = "gameStartBt";
-            this.gameStartBt.Size = new System.Drawing.Size(150, 100);
+            this.gameStartBt.Size = new System.Drawing.Size(225, 154);
             this.gameStartBt.TabIndex = 1;
             this.gameStartBt.Text = "Start";
             this.gameStartBt.UseVisualStyleBackColor = false;
@@ -202,61 +245,20 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(4, 12);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Location = new System.Drawing.Point(6, 18);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox2.Size = new System.Drawing.Size(960, 738);
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(33, 56);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(178, 21);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Detector Script:";
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(217, 53);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(520, 29);
-            this.comboBox2.TabIndex = 3;
-            this.comboBox2.Text = "camera_object_mono.py";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(77, 96);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 21);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Game Script:";
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.comboBox3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(218, 96);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(520, 29);
-            this.comboBox3.TabIndex = 5;
-            this.comboBox3.Text = "face_game_csharpReady.py";
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(857, 553);
+            this.ClientSize = new System.Drawing.Size(1286, 851);
             this.Controls.Add(this.tabControl1);
-            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Name = "Form1";
             this.Text = "Face Detector GUI";
             this.tabControl1.ResumeLayout(false);
